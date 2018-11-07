6ae558ff6003bd7fa9649a7e518e21160779d64b
xiaoliushifu <872140945@qq.com>
2018-11-07 23:34:47 +0800

    自己尝试定义一个服务提供者玩玩 为lumen如何扩展功能 这里就是扩展一个service而已，虽然作用不大，但是熟悉流程很关键 service完全可以像yii2里那样直接在控制器的方法里new即可。

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use Illuminate\Support\Facades\Request;
+use App\Http\Services\TestService;
 
 class TestController extends Controller
 {
@@ -12,6 +13,17 @@ class TestController extends Controller
     {
         $cars = Car::all();
         return response()->json($cars);
+    }
+
+    //利用框架的特性，直接注入一个Service，当然在Yii2中这样用也可以
+    //我们一般直接new
+    public function getTest()
+    {
+        //使用app()快捷函数从容器中实例化我们的对象，这种方式不用引入
+//        $test = app('test');
+        //直接实例化service当然需要引入
+        $test = new TestService();
+        $test->callMe('Test2Controller');
     }
 
     //访问List方法
