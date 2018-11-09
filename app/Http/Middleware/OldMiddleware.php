55887752501da7ef0abe885f2e313c730862f971
xiaoliushifu <872140945@qq.com>
2018-11-09 17:39:13 +0800

    实例化lumen的请求对象request之后，先要经过中间件才去判断路由 中间件的执行是通过pipeline这个对象的，很复杂暂时不考虑，以后再说吧。中间件的实例化都是走反射（非容器） 走完这个pipeline才是后面的路由判断。

--- app/Http/Middleware/OldMiddleware.php
+++ app/Http/Middleware/OldMiddleware.php
@@ -4,7 +4,7 @@
  * 实例中间件入门
  *
  * 中间件就是【请求】到达应用的一个个【关卡】，每个【关卡】都可以做些处理并决定【请求】
- * 是否可以继续往后走
+ * 是否可以继续走下去
  */
 namespace App\Http\Middleware;
 
@@ -21,6 +21,8 @@ class OldMiddleware
      */
     public function handle($request, Closure $next)
     {
+        Log::info('hello world');
+        Log::info($next,['next']);
 //        var_dump($request);
 //        if ($request->input('age') <= 200) {
 //            return redirect('foo');
